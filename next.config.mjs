35046c9964be2b5865d0f5443340bf5754beaeb8
imadjaha <imadjaha1@gmail.com>
2024-07-23 06:56:44 +0200

    added DarkMode

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,11 @@
 const nextConfig = {};
 
 export default nextConfig;
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  trailingSlash: true,
+};
