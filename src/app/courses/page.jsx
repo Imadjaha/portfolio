18e3238e163455d73effe07d58d29a46bd8d7d11
imadjaha <imadjaha1@gmail.com>
2024-07-26 10:06:21 +0200

    added Courses

--- src/app/courses/page.jsx
+++ src/app/courses/page.jsx
@@ -1,14 +1,18 @@
 'use client';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import Image from 'next/image';
 import courses from './courses'; 
 import './courses.css'; 
+import scrollTopButton from './assets/up-arrow.png';
 
 export default function Courses() {
 
 
 
 const [searchQuery, setSearchQuery] = useState('');
+const [expandedCards, setExpandedCards] = useState([]);
+const [showScrollButton, setShowScrollButton] = useState(false);
+
 const handleSearchChange = (event) => {
   setSearchQuery(event.target.value.toLowerCase());
 }
@@ -22,12 +26,33 @@ course.semester.toLowerCase().includes(searchQuery) ||
 course.description.toLowerCase().includes(searchQuery)
 );
 
+const handleReadMore = (id) => {
+  setExpandedCards((prevExpandedCards) => 
+    prevExpandedCards.includes(id) ? prevExpandedCards.filter((cardId) => cardId !== id) : [...prevExpandedCards, id]
+  );
+  };
+
+  // Scroll to the card if it is being expanded
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowScrollButton(window.scrollY > 200); // Show button after scrolling down 200px
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
 
 
   return (
     <div>
 
     <input
+    
       type="text"
       placeholder="Search keywords..."
       value={searchQuery}
@@ -38,16 +63,27 @@ course.description.toLowerCase().includes(searchQuery)
 <div className='cardContainer1'>
         {filteredCourses.length > 0 ? (
           filteredCourses.map((course) => (
-            <div key={course.id} className="card1">
+            <div key={course.id} className={`card1 ${expandedCards.includes(course.id)? 'expanded' : 'truncated'}`}>
               <h2 className="cardTitle">{course.title}</h2>
               <span className='cardShortcut'>{course.semester}</span>
-              <p className="cardDescription">{course.description}</p>
+              <p className='cardDescription'>
+              {expandedCards.includes(course.id) ? course.description : (course.title.length > 43 ? `${course.description.substring(0, 120)}....` : `${course.description.substring(0, 220)}....`)}
+                <span className="readMore" onClick={() => handleReadMore(course.id)}>
+                  {expandedCards.includes(course.id) ? 'Read less' : 'Read more'}
+                  </span>
+                  </p>
             </div>
           ))
         ) : (
           <p>No Result found</p>
         )}
     </div>
+    {showScrollButton && (
+  <button onClick={scrollToTop} className="scrollToTopButton">
+    <Image src={scrollTopButton} alt="Scroll to top button" width={50} height={50} />
+  </button>
+)}
+
 
     </div>
   );
