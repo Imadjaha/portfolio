d74a8c1cd0ef146304ecf747b4caa15a50950d72
Imadjaha <imadjaha1@gmail.com>
2025-04-16 09:08:23 +0200

    bug fix: fix: update internal links for GitHub Pages routing compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
-// next.config.mjs
-// const isProd = process.env.NODE_ENV === 'production';
+next.config.mjs
+const isProd = process.env.NODE_ENV === 'production';
 
-// const nextConfig = {
-//   assetPrefix: isProd ? '/portfolio/' : '',
-//   basePath: isProd ? '/portfolio' : '',
-//   trailingSlash: true,
-// };
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-
-
+  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: isProd ? '/portfolio' : '',
+  trailingSlash: true,
 };
+
 
 export default nextConfig;
